67a25076b4eeea7f08391da33e076da2a7c68f66
pawanappikatla <141493522+pawanappikatla@users.noreply.github.com>
2023-08-25 11:41:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "Hello World"
+    return "Hello pawan"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
